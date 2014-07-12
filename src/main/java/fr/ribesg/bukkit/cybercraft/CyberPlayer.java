f0eae3c7aaa24343248c9fa662b9986316626dd7
Ribesg <Ribesg@yahoo.fr>
2014-07-12 12:31:45 +0200

    Add CyberPlayer class

---
+++ src/main/java/fr/ribesg/bukkit/cybercraft/CyberPlayer.java
@@ -0,0 +1,104 @@
+package fr.ribesg.bukkit.cybercraft;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+/**
+ * Represents a CyberPlayer
+ */
+public class CyberPlayer {
+
+	/**
+	 * Player's UUID
+	 */
+	private final UUID playerId;
+
+	/**
+	 * Player's last known name
+	 */
+	private final String playerName;
+
+	/**
+	 * Player's power
+	 */
+	private long power;
+
+	/**
+	 * Builds a CyberPlayer from a Player and an initial Power.
+	 *
+	 * @param player       the player
+	 * @param initialPower the initial power
+	 */
+	public CyberPlayer(final Player player, final long initialPower) {
+		this(player.getUniqueId(), player.getName(), initialPower);
+	}
+
+	/**
+	 * Builds a CyberPlayer from its base data, usually from saved state.
+	 *
+	 * @param id    the player's UUID
+	 * @param name  the player's name
+	 * @param power the player's power level
+	 */
+	public CyberPlayer(final UUID id, final String name, final long power) {
+		this.playerId = id;
+		this.playerName = name;
+		this.power = power;
+	}
+
+	/**
+	 * Recharge this CyberPlayer.
+	 *
+	 * @param amount the amount of power to add
+	 */
+	public void recharge(final int amount) {
+		this.power += amount;
+	}
+
+	/**
+	 * Discharge this CyberPlayer.
+	 *
+	 * @param amount the amount of power to remove
+	 */
+	public void discharge(final int amount) {
+		this.power -= amount;
+		if (this.power < 0) {
+			this.power = 0;
+			noMorePower();
+		}
+	}
+
+	/**
+	 * Called when the Player no longer has power. Handles mostly-inevitable death.
+	 */
+	public void noMorePower() {
+		// TODO Handle Player slow death
+	}
+
+	/**
+	 * Gets the Player's UUID.
+	 *
+	 * @return the Player's UUID
+	 */
+	public UUID getPlayerId() {
+		return playerId;
+	}
+
+	/**
+	 * Gets the last known Player's name.
+	 *
+	 * @return the last known Player's name
+	 */
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	/**
+	 * Gets the Player's power level.
+	 *
+	 * @return the Player's power level
+	 */
+	public long getPower() {
+		return power;
+	}
+}
